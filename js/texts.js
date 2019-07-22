a012147855f0dbfb4ec67452998c6c4e3e46a96a
duxy1996 <durancdr96@gmail.com>
2019-07-22 21:04:53 +0200

    Pages - Added "translation logic"

---
+++ js/texts.js
@@ -0,0 +1,8 @@
+
+var beewo_text_str_es = "La primera startUp europea en fabricar drones modulares. En kits de aprendizaje , los proyectos de Beewo pretenden acercar la programacion a los jóvenes. Beewo no es solo un proyecto educativo es una nueva forma de aprender y compartir"
+var beewo_text_str_en = "Beewo is the first European startup which made modular DeferredPermissionRequest. Beewo tries to teach programming and maths to the children with our -Builder Kits-. Beewo is not only a educative project is a new way to learn and share"
+
+function translateLabels()
+{
+  document.getElementById("beewo_text").innerHTML = beewo_text_str_en;
+}
