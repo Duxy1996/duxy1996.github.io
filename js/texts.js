b2dcb91a0177130fd9e42b108401e0660c1dee9c
duxy1996 <durancdr96@gmail.com>
2019-07-22 21:42:08 +0200

    Pages - Added basic logic to translate the texts in the web page

--- js/texts.js
+++ js/texts.js
@@ -2,7 +2,24 @@
 var beewo_text_str_es = "La primera startUp europea en fabricar drones modulares. En kits de aprendizaje , los proyectos de Beewo pretenden acercar la programacion a los jóvenes. Beewo no es solo un proyecto educativo es una nueva forma de aprender y compartir"
 var beewo_text_str_en = "Beewo is the first European startup which made modular DeferredPermissionRequest. Beewo tries to teach programming and maths to the children with our -Builder Kits-. Beewo is not only a educative project is a new way to learn and share"
 
+var spain = true;
+
 function translateLabels()
 {
-  document.getElementById("beewo_text").innerHTML = beewo_text_str_en;
+  spain = !spain;
+
+  if (spain)
+  {
+    document.getElementById("beewo_text").innerHTML = beewo_text_str_es;
+    document.getElementById("flag").classList.add('english');
+    document.getElementById("flag").classList.remove('spain');
+  }
+  else
+  {
+    document.getElementById("beewo_text").innerHTML = beewo_text_str_en;
+    document.getElementById("flag").classList.add('spain');
+    document.getElementById("flag").classList.remove('english');
+  }
+
+
 }
