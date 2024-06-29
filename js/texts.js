adfacb410b4b1c6b7206cfa36612f626e7719063
Carlos Duran <durancdr96@gmail.com>
2024-06-29 17:32:29 +0200

    [CSS] - Fixed minor issues with naming.

--- js/texts.js
+++ js/texts.js
@@ -5,7 +5,7 @@ var beewo_text_str_en = "Beewo is the first European startup which made modular 
 var infusion_text_str_es = "Formo parte de un equipo multidisciplinar que intenta acercar la tecnología a todo el mundo. Hemos desarrollado una web en Ruby on Rails donde cualquier persona puede subir su MeetUp. Cada dos semanas estos se imparten según los resultados de las votaciones"
 var infusion_text_str_en = "I am a member of a multidisciplinar team which tries to draw near technology to everybody. We have develop a web page using Ruby on Rails where any person can upload its Meetup. Each two weeks there are a Meetup baseed on the votations results"
 
-var spain = false;
+var spain = true;
 
 function translateLabels()
 {
@@ -16,13 +16,13 @@ function translateLabels()
     document.getElementById("beewo_text").innerHTML = beewo_text_str_es;
     document.getElementById("infusion_text").innerHTML = infusion_text_str_es;
     document.getElementById("flag").classList.add('english');
-    document.getElementById("flag").classList.remove('spain');
+    document.getElementById("flag").classList.remove('spanish');
   }
   else
   {
     document.getElementById("beewo_text").innerHTML = beewo_text_str_en;
     document.getElementById("infusion_text").innerHTML = infusion_text_str_en;
-    document.getElementById("flag").classList.add('spain');
+    document.getElementById("flag").classList.add('spanish');
     document.getElementById("flag").classList.remove('english');
   }
 }
